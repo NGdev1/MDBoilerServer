5deee73baf8b24431663a482cfb666575d589d26
Andreichev Michael <andreichev.m@mail.ru>
2020-02-24 16:16:40 +0300

    first commit

--- src/main/java/com/md/mdboiler/MDBoilerApplication.java
+++ src/main/java/com/md/mdboiler/MDBoilerApplication.java
@@ -2,8 +2,10 @@ package com.md.mdboiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.md.mdboiler"})
 public class MDBoilerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MDBoilerApplication.class, args);
